149cc5c86ef9cdc9d5a9cdefef8ba61dcfcbbfb7
John-Patrik <jpatrik.nilsson+cc@gmail.com>
2011-03-27 14:36:15 +0200

    JS toolbars.

--- application.js
+++ application.js
@@ -21,7 +21,57 @@ Ext.setup({
     icon: 'icon.png',
     glossOnIcon: false,
     onReady: function() {
+
+   // For the new tab bar
+
+      var tapHandler = function(button, event) {
+         var txt = "User tapped the '" + button.text + "' button.";
+         Ext.getCmp('toolbartxt').update(txt);
+      }
         
+      var buttonsGroup1 = [{
+            text: 'Back',
+            ui: 'back',
+            handler: tapHandler
+        }, {
+            text: 'Default',
+            handler: tapHandler
+        }, {
+            text: 'Round',
+            ui: 'round',
+            handler: tapHandler
+        }];
+
+      var buttonsGroup2 = [{
+            text: 'Back',
+            ui: 'back',
+            handler: tapHandler
+        }, {
+            text: 'Default',
+            handler: tapHandler
+        }, {
+            text: 'Round',
+            ui: 'round',
+            handler: tapHandler
+        }];
+
+         var dockedItems = [{
+                xtype: 'toolbar',
+                // dock this toolbar at the top
+                dock: 'top',
+                //items: buttonsGroup1.concat(buttonsGroup2).concat(buttonsGroup3).concat(buttonsGroup4)
+                items: buttonsGroup1
+            }, {
+                xtype: 'toolbar',
+                // dock this toolbar at the bottom
+                ui: 'light',
+                dock: 'bottom',
+                //items: buttonsGroup1.concat(buttonsGroup2).concat(buttonsGroup3).concat(buttonsGroup4)
+                items: buttonsGroup2
+            }];
+
+   // end tab bar
+
          var map = new Ext.Map({
             title: 'Map',
             getLocation: true,
@@ -33,6 +83,7 @@ Ext.setup({
         var panel = new Ext.TabPanel({
             fullscreen: true,
             cardSwitchAnimation: 'slide',
+            dockedItems: dockedItems,
             items: [map]
         });
     }
