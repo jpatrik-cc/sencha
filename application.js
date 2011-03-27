5764ed0e8113aa7163006b9b55e89f2a5f71c4c2
Nicolas Lara <nicolaslara@gmail.com>
2011-03-27 15:08:49 +0200

    separated buttons

--- application.js
+++ application.js
@@ -27,60 +27,78 @@ Ext.setup({
       var tapHandler = function(button, event) {
          var txt = "User tapped the '" + button.text + "' button.";
          Ext.getCmp('toolbartxt').update(txt);
-      }
-        
+      };
+
       var buttonsGroup1 = [{
-            text: 'Back',
-            ui: 'back',
-            handler: tapHandler
-        }, {
-            text: 'Default',
-            handler: tapHandler
-        }, {
-            text: 'Round',
-            ui: 'round',
+            text: 'OK',
+            ui: 'next',
             handler: tapHandler
         }];
 
       var buttonsGroup2 = [{
-            text: 'Back',
-            ui: 'back',
-            handler: tapHandler
-        }, {
-            text: 'Default',
-            handler: tapHandler
-        }, {
-            text: 'Round',
-            ui: 'round',
-            handler: tapHandler
-        }];
+        text: 'Button1',
+        ui: 'next',
+        handler: tapHandler
+      },
+      {
+        text: 'Button2',
+        ui: 'next',
+        handler: tapHandler
+      }];
+
+      // var Icons = new Ext.TabPanel({
+      //   items: [{
+      //             iconCls: 'bookmarks',
+      //             title: 'Bookmarks',
+      //             html: 'Both toolbars and tabbars have built-in, ready to use icons. Styling custom icons is no hassle.<p><small>If you can&#8217;t see all of the buttons below, try scrolling left/right.</small></p>'
+      //           }, {
+      //             iconCls: 'download',
+      //             title: 'Download',
+      //             html: 'Pressed Download'
+      //           }, {
+      //             iconCls: 'favorites',
+      //             title: 'Favorites',
+      //             html: 'Pressed Favorites'
+      //           }, {
+      //             iconCls: 'info',
+      //             title: 'Info',
+      //             html: 'Pressed Info'
+      //           }]});
 
          var dockedItems = [{
                 xtype: 'toolbar',
-                // dock this toolbar at the top
                 dock: 'top',
-                //items: buttonsGroup1.concat(buttonsGroup2).concat(buttonsGroup3).concat(buttonsGroup4)
                 items: buttonsGroup1
             }, {
                 xtype: 'toolbar',
-                // dock this toolbar at the bottom
                 ui: 'light',
                 dock: 'bottom',
-                //items: buttonsGroup1.concat(buttonsGroup2).concat(buttonsGroup3).concat(buttonsGroup4)
                 items: buttonsGroup2
-            }];
+            },
+         ];
 
    // end tab bar
+      var map2 = new Ext.Map({
+            title: 'Map2',
+            getLocation: true,
+            mapOptions: {
+                zoom: 12
+            },
+            margin:100,
+            draggable:false,
+            fullscreen:false
+        });
 
-         var map = new Ext.Map({
+
+      var map = new Ext.Map({
             title: 'Map',
             getLocation: true,
             mapOptions: {
                 zoom: 12
-            }
+            },
         });
 
-        var panel = new Ext.TabPanel({
+        var panel = new Ext.Panel({
             fullscreen: true,
             cardSwitchAnimation: 'slide',
             dockedItems: dockedItems,
