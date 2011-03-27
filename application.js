a3b865bacef53b2d23e57bbe14001a67247fe6dd
Nicolas Lara <nicolaslara@gmail.com>
2011-03-27 17:33:05 +0200

    Fixed general UI

--- application.js
+++ application.js
@@ -1,20 +1,3 @@
-
-/*
-var mapScreen = new Ext.Component({
-    title: 'Location',
-    getLocation: true,
-    mapOptions: {
-       zoom: 12
-    }
-});
-
-var panel = new Ext.TabPanel({
-    fullscreen: true,
-    cardAnimation: 'slide',
-    items: [mapScreen]
-});
-*/
-
 Ext.setup({
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
@@ -31,78 +14,75 @@ Ext.setup({
 
       var buttonsGroup1 = [{
             text: 'OK',
-            ui: 'next',
+            ui: 'forward',
             handler: tapHandler
         }];
-
-      var buttonsGroup2 = [{
-        text: 'Button1',
-        ui: 'next',
-        handler: tapHandler
-      },
-      {
-        text: 'Button2',
-        ui: 'next',
-        handler: tapHandler
-      }];
-
-      // var Icons = new Ext.TabPanel({
-      //   items: [{
-      //             iconCls: 'bookmarks',
-      //             title: 'Bookmarks',
-      //             html: 'Both toolbars and tabbars have built-in, ready to use icons. Styling custom icons is no hassle.<p><small>If you can&#8217;t see all of the buttons below, try scrolling left/right.</small></p>'
-      //           }, {
-      //             iconCls: 'download',
-      //             title: 'Download',
-      //             html: 'Pressed Download'
-      //           }, {
-      //             iconCls: 'favorites',
-      //             title: 'Favorites',
-      //             html: 'Pressed Favorites'
-      //           }, {
-      //             iconCls: 'info',
-      //             title: 'Info',
-      //             html: 'Pressed Info'
-      //           }]});
-
-         var dockedItems = [{
-                xtype: 'toolbar',
-                dock: 'top',
-                items: buttonsGroup1
-            }, {
-                xtype: 'toolbar',
-                ui: 'light',
-                dock: 'bottom',
-                items: buttonsGroup2
-            },
-         ];
-
-   // end tab bar
-      var map2 = new Ext.Map({
-            title: 'Map2',
-            getLocation: true,
-            mapOptions: {
-                zoom: 12
-            },
-            margin:100,
-            draggable:false,
-            fullscreen:false
-        });
-
 
       var map = new Ext.Map({
             title: 'Map',
             getLocation: true,
             mapOptions: {
                 zoom: 12
-            },
+            }
         });
 
-        var panel = new Ext.Panel({
+
+        //This is going to be our main UI panel where most interactions will happen
+        var ui_panel = new Ext.Panel({
             fullscreen: true,
+            title: 'Park',
+            iconCls: 'download',
+            dockedItems:[{
+                xtype: 'toolbar',
+                dock: 'top',
+                items: buttonsGroup1
+            }],
             cardSwitchAnimation: 'slide',
-            dockedItems: dockedItems,
             items: [map]
         });
+
+      var current_panel = {
+        iconCls: 'time',
+        title: 'Parkings',
+        html: 'Infor about your currently parked cars'
+      };
+
+      var cars_panel = {
+        iconCls: 'bookmarks',
+        title: 'Cars',
+        html: 'Pressed Cars'
+      };
+
+
+      var user_panel = {
+        iconCls: 'user',
+        title: 'User',
+        html: 'Pressed User'
+      };
+
+      var panel = new Ext.TabPanel(
+      {
+        defaults: {
+          cls: 'card card3'
+        },
+        fullscreen:true,
+        items: [
+          ui_panel,
+          cars_panel,
+          current_panel,
+          user_panel
+        ],
+
+        tabBar: {
+          dock: 'bottom',
+          scroll: {
+            direction: 'horizontal',
+            useIndicators: false
+          },
+          layout: {
+            pack: 'center'
+          }
+        }
+      });
     }
 });
