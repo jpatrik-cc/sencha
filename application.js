972bd0b3d93cc5e2341d38576f0b5138acfca827
John-Patrik <jpatrik.nilsson+cc@gmail.com>
2011-03-27 17:35:31 +0200

    Two bars at the top.

--- application.js
+++ application.js
@@ -1,108 +1,96 @@
-
-/*
-var mapScreen = new Ext.Component({
-    title: 'Location',
-    getLocation: true,
-    mapOptions: {
-       zoom: 12
-    }
-});
-
-var panel = new Ext.TabPanel({
-    fullscreen: true,
-    cardAnimation: 'slide',
-    items: [mapScreen]
-});
-*/
-
 Ext.setup({
-    tabletStartupScreen: 'tablet_startup.png',
-    phoneStartupScreen: 'phone_startup.png',
-    icon: 'icon.png',
-    glossOnIcon: false,
-    onReady: function() {
+   tabletStartupScreen: 'tablet_startup.png',
+   phoneStartupScreen: 'phone_startup.png',
+   icon: 'icon.png',
+   glossOnIcon: false,
+   onReady: function() {
 
-   // For the new tab bar
-
+      // Event handler for the buttons...
       var tapHandler = function(button, event) {
          var txt = "User tapped the '" + button.text + "' button.";
          Ext.getCmp('toolbartxt').update(txt);
       };
 
-      var buttonsGroup1 = [{
-            text: 'OK',
-            ui: 'next',
-            handler: tapHandler
-        }];
+      // These are groups of buttons... 
+      var topButtonGroup = [
+         {
+         text: 'Forward',
+         ui: 'forward',
+         handler: tapHandler
+      }
+      ];
 
-      var buttonsGroup2 = [{
-        text: 'Button1',
-        ui: 'next',
-        handler: tapHandler
+      var bottomButtonGroup = [
+         {
+         text: 'Back',
+         ui: 'back',
+         handler: tapHandler
       },
       {
-        text: 'Button2',
-        ui: 'next',
-        handler: tapHandler
-      }];
+         text: 'Other',
+         ui: 'default',
+         handler: tapHandler
+      }
+      ];
 
-      // var Icons = new Ext.TabPanel({
-      //   items: [{
-      //             iconCls: 'bookmarks',
-      //             title: 'Bookmarks',
-      //             html: 'Both toolbars and tabbars have built-in, ready to use icons. Styling custom icons is no hassle.<p><small>If you can&#8217;t see all of the buttons below, try scrolling left/right.</small></p>'
-      //           }, {
-      //             iconCls: 'download',
-      //             title: 'Download',
-      //             html: 'Pressed Download'
-      //           }, {
-      //             iconCls: 'favorites',
-      //             title: 'Favorites',
-      //             html: 'Pressed Favorites'
-      //           }, {
-      //             iconCls: 'info',
-      //             title: 'Info',
-      //             html: 'Pressed Info'
-      //           }]});
+      // These are docked to the "main" Panel...
+      var dockedItems = [
+         {
+         xtype: 'toolbar',
+         dock: 'top',
+         title: 'Location'
+      },
+      {
+         xtype: 'toolbar',
+         dock: 'top',
+         ui: 'light',
+         align: 'right',
+         items: topButtonGroup
+      }, 
+      {
+         xtype: 'toolbar',
+         ui: 'light',
+         dock: 'bottom',
+         items: bottomButtonGroup
+      },
+      ];
 
-         var dockedItems = [{
-                xtype: 'toolbar',
-                dock: 'top',
-                items: buttonsGroup1
-            }, {
-                xtype: 'toolbar',
-                ui: 'light',
-                dock: 'bottom',
-                items: buttonsGroup2
-            },
-         ];
-
-   // end tab bar
+      // These are items included in the "main" Panel...
       var map2 = new Ext.Map({
-            title: 'Map2',
-            getLocation: true,
-            mapOptions: {
-                zoom: 12
-            },
-            margin:100,
-            draggable:false,
-            fullscreen:false
-        });
-
+         title: 'Map2',
+         getLocation: true,
+         mapOptions: {
+            zoom: 12
+         },
+         margin:100,
+         draggable:false,
+         fullscreen:false
+      });
 
       var map = new Ext.Map({
-            title: 'Map',
-            getLocation: true,
-            mapOptions: {
-                zoom: 12
-            },
-        });
+         title: 'Map',
+         getLocation: true,
+         mapOptions: {
+            zoom: 12
+         },
+      });
 
-        var panel = new Ext.Panel({
-            fullscreen: true,
-            cardSwitchAnimation: 'slide',
-            dockedItems: dockedItems,
-            items: [map]
-        });
-    }
+      var panel = new Ext.Panel({
+         fullscreen: true,
+         cardSwitchAnimation: 'slide',
+         dockedItems: dockedItems,
+         defaults: {
+            scroll: 'vertical',
+            xtype: 'panel',
+            layout: 'hbox',
+            pack: 'justify',
+            align: 'center',
+            defaults: {
+               xtype: 'button',
+               ui: 'confirm',
+            }
+         },
+         items: [map]
+      });
+   }
 });
