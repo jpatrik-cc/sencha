bd343d7e04e2223ea072a8d93f1c55ab272a3ad7
Nicolas Lara <nicolaslara@gmail.com>
2011-03-29 09:58:19 +0200

    added dummy models and controllers

---
+++ app/controllers/paymentOptions.js
@@ -0,0 +1,9 @@
+// This needs to be re-defined
+app.controllers.paymentOptions = new Ext.Controller({
+    index: function(options) {
+    },
+    show: function(options) {
+    },
+    edit: function(options) {
+    }
+});
