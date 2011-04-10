f0679a0f61571c608c20a1959abe7d114e1ebe89
michael404 <michael404@gmail.com>
2011-04-10 06:08:13 -0700

    Edited app/views/ParkingWorkflow/LocationScreen.js via GitHub

--- app/views/ParkingWorkflow/LocationScreen.js
+++ app/views/ParkingWorkflow/LocationScreen.js
@@ -21,7 +21,7 @@ app.views.parkingWorkflow.LocationScreen = Ext.extend(Ext.Panel, {
                     locationupdate: function (geo) {
                         img = document.createElement('img');
                         img.setAttribute('style','border-radius: 5px;');
-                        img.setAttribute('src','http://maps.google.com/maps/api/staticmap?markers='+geo.latitude+','+geo.longitude+'&zoom=16&size=300x160&sensor=true');
+                        img.setAttribute('src','http://maps.google.com/maps/api/staticmap?markers='+geo.latitude+','+geo.longitude+'&zoom=16&size=300x140&sensor=true');
                         div = document.getElementById("map_img");
                         div.appendChild(img);
                     },
